65a07f9a120bf19a167c09c4651b47500aab378b
Caroline Shields <cbshields@outlook.com>
2018-03-02 21:22:22 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,4 +28,9 @@ class SongsController < ApplicationController
     @song = Song.update(params.require(:song))
     redirect_to song_path(@song)
   end
+
+  private
+    def song_params
+      params.require(:song).permit(:name,:artist_id,:genre_id)
+    end
 end #ends SongsController
